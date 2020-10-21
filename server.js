33d5106e827ee1f147ce79d2cdc40f5755a6c87a
masudrana08 <rana028511@gmail.com>
2020-10-21 10:39:03 +0600

    ready to host

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ client.connect(err => {
 });
 
 
-const PORT = process.env.port || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT,()=>{
     console.log('server is running with '+PORT+' port')
 })
